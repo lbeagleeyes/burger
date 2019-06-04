c40642642a7229cf4604993e2fe9b0beb6574cd9
Laura B <laurabermudezg@gmail.com>
2019-06-03 23:35:16 -0700

    added jawsdb sql connection

--- config/connection.js
+++ config/connection.js
@@ -39,8 +39,6 @@
 //   }
 // }
 
-
-
 // module.exports = DBConnection;
 
 
@@ -49,16 +47,22 @@ var keys = require("./keys.js");
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: keys.dbInfo.psswd,
-  database: keys.dbInfo.dbName
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: keys.dbInfo.psswd,
+    database: keys.dbInfo.dbName
+  });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
